1a127ade7ed558df31fd57251a3e0c3d7f21e6f1
MauricioAlonso <program.alonso@gmail.com>
2023-11-13 18:12:05 -0300

    creado el modelo de ticket

---
+++ src/dao/ticketsMongoDao.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const TicketsModelo = require("./DB/models/ticket.modelo.js");
+
+class TicketsMongoDao {
+  
+  async listarTickets() {
+    try {
+      const tickets = await TicketsModelo.find();
+      return tickets;
+    } catch (error) {
+      throw new Error("Error al obtener tickets desde la base de datos");
+    }
+  }
+  async obtenerTicket(id) {
+    return await TicketsModelo.findById(id).lean();
+  }
+
+  async crearTicket(producto) {
+    return await TicketsModelo.create(producto);
+  }
+
+  async existeTicket(code) {
+    return await TicketsModelo.findOne({ code: code });
+  }
+
+  async borrarTicket(id) {
+    return await TicketsModelo.deleteOne({ _id: id });
+  }
+}
+
+module.exports = new TicketsMongoDao();
