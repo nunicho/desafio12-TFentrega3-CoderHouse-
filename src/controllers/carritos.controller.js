db8bb21164868950cea2a000edbc6ef4aabb9b9c
MauricioAlonso <program.alonso@gmail.com>
2023-11-13 02:01:17 -0300

    listo el mongoDao de carritos

--- src/controllers/carritos.controller.js
+++ src/controllers/carritos.controller.js
@@ -1,3 +1,44 @@
+
+
+const mongoose = require("mongoose");
+//const carritosServices = require("../services/carritos.service.js");
+const carritosMongoDao = require("../dao/carritosMongoDao.js");
+
+const obtenerCarrito = async (req, res, next) => {
+  const cid = req.params.cid;
+
+  if (!mongoose.Types.ObjectId.isValid(cid)) {
+    return res.status(400).json({
+      status: "error",
+      mensaje: 'Requiere un argumento "cid" de tipo ObjectId válido',
+    });
+  }
+
+  try {
+    const carrito = await carritosMongoDao.obtenerCarritoPorId(cid);
+
+    if (!carrito) {
+      return res.status(404).json({
+        status: "error",
+        mensaje: `El carrito con ID ${cid} no existe`,
+      });
+    }
+
+    res.locals.carritoDB = carrito;
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+};
+
+module.exports = {
+  obtenerCarrito,
+};
+
+
+
+/*
 const carritosServices = require("../services/carritos.service.js");
 
 const obtenerCarrito = async (req, res, next) => {
@@ -12,3 +53,4 @@ const obtenerCarrito = async (req, res, next) => {
 module.exports = {
   obtenerCarrito,
 };
+*/
---
+++ src/dao/carritosMongoDao.js
@@ -0,0 +1,22 @@
+const Carrito = require("./DB/models/carritos.modelo.js");
+const Producto = require("./DB/models/productos.modelo.js");
+
+const carritosMongoDao = {
+  async obtenerCarritoPorId(id) {
+    try {
+      const carrito = await Carrito.findOne({ _id: id })
+        .populate({
+          path: "productos.producto",
+          model: Producto,
+        })
+        .lean();
+      return carrito;
+    } catch (error) {
+      throw new Error("Error al obtener el carrito desde la base de datos");
+    }
+  },
+
+  // Puedes agregar más métodos según tus necesidades, como crearCarrito, actualizarCarrito, eliminarCarrito, etc.
+};
+
+module.exports = carritosMongoDao;
